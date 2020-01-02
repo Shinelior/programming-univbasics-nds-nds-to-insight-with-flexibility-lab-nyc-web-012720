6f2d8c3ac3d6ccafc05ad3049e8e4aaab13fece6
lior Shine <shinelior@gmail.com>
2020-01-02 17:44:50 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -108,8 +108,8 @@ def movies_with_directors_set(source)
   
  while row_index<source.length  do
    binding.pry
-    director_namee=
-    new_hash[:director_name]=source[row_index][:name]
+    director_name=source[row_index][:name]
+    
     new_hash[:title]=source[row_index][:movies][colunm_index][:title]
       new_hash[:title]=source[row_index][:movies][colunm_index][:title]
       new_array<< movies_with_director_key(name, movies_collection)
